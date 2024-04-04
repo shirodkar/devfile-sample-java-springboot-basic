540cd1291a9d7b6f24ccb523ca6afdb59a512aec
shirodkar <shirodkar.sachin@gmail.com>
2024-04-04 19:56:51 -0400

    enabled refresh

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,14 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class DemoApplication {
 
     @Value( "${name}" )
